e69c2a6f4e3b4b5a63287a18d4cd39115a395313
Darian-Frey <shane.hartley06@gmail.com>
2021-10-09 21:52:31 +0000

    Added templates and logic

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets Learn Python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
 
 
 def home(request):
     """Home Page"""
-    return HttpResponse('Home Page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
 
-def room(request):
+def room(request, pk):
     """Room Page"""
-    return HttpResponse('Room')
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -56,7 +57,9 @@ ROOT_URLCONF = 'studybud.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
